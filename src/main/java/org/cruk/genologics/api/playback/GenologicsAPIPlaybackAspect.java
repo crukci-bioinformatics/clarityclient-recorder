9d124ac6a9077df07133db1c8ca60cd7dcd43deb
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2019-09-25 15:56:06 +0100

    Need a specific join point around loadAll.

--- src/main/java/org/cruk/genologics/api/playback/GenologicsAPIPlaybackAspect.java
+++ src/main/java/org/cruk/genologics/api/playback/GenologicsAPIPlaybackAspect.java
@@ -31,8 +31,10 @@ import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -261,7 +263,7 @@ public class GenologicsAPIPlaybackAspect
      * code if the file exists, or one with no body and a "not found" status if it
      * does not.
      *
-     * @throws Throwable if there is anything fails.
+     * @throws Throwable if there is anything that fails.
      */
     public ResponseEntity<?> doGetEntity(ProceedingJoinPoint pjp) throws Throwable
     {
@@ -278,6 +280,39 @@ public class GenologicsAPIPlaybackAspect
         }
 
         return response;
+    }
+
+    /**
+     * Join point around calls to the API's {@code loadAll} method. Loads each
+     * entity in the list of links from the messages directory. At this point
+     * the link object provides the type and the URI.
+     *
+     * @param pjp The join point.
+     *
+     * @return A list of the entities loaded from the links.
+     *
+     * @throws FileNotFoundException if there is no entity file recorded for any of the links.
+     *
+     * @throws Throwable if there is anything else that fails.
+     */
+    public List<?> doLoadAll(ProceedingJoinPoint pjp) throws Throwable
+    {
+        Collection<?> links = (Collection<?>)pjp.getArgs()[0];
+        List<Object> replies = new ArrayList<Object>(links.size());
+        Iterator<?> iter = links.iterator();
+        while (iter.hasNext())
+        {
+            LimsLink<?> link = (LimsLink<?>)iter.next();
+            File file = getFileForEntity(link.getEntityClass(), link.getUri());
+
+            if (!file.exists())
+            {
+                throw new FileNotFoundException("There is no file " + file.getName() + " recorded.");
+            }
+
+            replies.add(jaxbMarshaller.unmarshal(new StreamSource(file)));
+        }
+        return replies;
     }
 
     /**
