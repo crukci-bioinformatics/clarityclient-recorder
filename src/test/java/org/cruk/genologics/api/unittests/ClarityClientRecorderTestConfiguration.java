fee8b13635c6803f6390b0c12606b7d6e8be0e21
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2021-04-08 14:17:17 +0100

    Need to define genologicsAPI bean as no longer in the main definition file.

--- src/test/java/org/cruk/genologics/api/unittests/ClarityClientRecorderTestConfiguration.java
+++ src/test/java/org/cruk/genologics/api/unittests/ClarityClientRecorderTestConfiguration.java
@@ -22,13 +22,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.io.IOUtils;
 import org.cruk.genologics.api.GenologicsAPI;
+import org.cruk.genologics.api.impl.GenologicsAPIImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -39,18 +38,16 @@ import org.springframework.context.annotation.ImportResource;
 })
 public abstract class ClarityClientRecorderTestConfiguration
 {
-    @Autowired
-    protected GenologicsAPI api;
-
     public ClarityClientRecorderTestConfiguration()
     {
     }
 
-    @PostConstruct
-    public void setCredentialsOnApi()
+    @Bean
+    public GenologicsAPI genologicsAPI()
     {
-        InputStream propsIn = getClass().getResourceAsStream("/testcredentials.properties");
+        GenologicsAPIImpl api = new GenologicsAPIImpl();
 
+        InputStream propsIn = getClass().getResourceAsStream("/testcredentials.properties");
         if (propsIn != null)
         {
             try
@@ -69,5 +66,7 @@ public abstract class ClarityClientRecorderTestConfiguration
                 IOUtils.closeQuietly(propsIn);
             }
         }
+
+        return api;
     }
 }
