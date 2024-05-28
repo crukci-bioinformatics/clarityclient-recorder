8ca2a2b71b06ec840824ef6e330cc8e611a00042
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2024-05-28 16:06:26 +0100

    Added necessary 'opens' declarations.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -33,4 +33,8 @@ module org.cruk.clarity.api.recorder
     exports org.cruk.clarity.api.record;
     exports org.cruk.clarity.api.record.spring;
     exports org.cruk.clarity.api.search;
+
+    opens org.cruk.clarity.api.playback.spring to spring.core;
+    opens org.cruk.clarity.api.record.spring to spring.core;
+    opens org.cruk.clarity.api.search to spring.core, jakarta.xml.bind;
 }
